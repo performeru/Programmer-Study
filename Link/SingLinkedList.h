06da22310cf0211a305e005e01b1010ec7b99bf4
performeru <130340605+performeru@users.noreply.github.com>
2023-07-10 18:50:48 +0900

    Create SingLinkedList.h

---
+++ Link/SingLinkedList.h
@@ -0,0 +1,23 @@
+#pragma once
+#include "Monster.h"
+
+struct MonsterList
+{
+	Monster* pHead{};
+	Monster* pTail{};
+};
+
+
+Monster* CreateMonster(MonsterList& list, const char* name, const int hp);
+
+int GetMonsterCount(const MonsterList& list);
+
+void PrintMonstersR(Monster* p);
+
+void PrintMonsters(const MonsterList& list);
+
+Monster* FindMonster(const MonsterList& list, const char* name);
+
+void DeleteAll(MonsterList& list);
+
+bool DeleteMonser(MonsterList& list, const char* name);
