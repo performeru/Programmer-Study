7924b0f70cf0c121817ea040bce70b4e11b9c811
performeru <130340605+performeru@users.noreply.github.com>
2023-08-06 20:53:47 +0900

    Update Inheritance and Polymorphism: Stack, Queue.cpp

--- Programmer Study Problem Practice/Inheritance and Polymorphism: Stack, Queue.cpp
+++ Programmer Study Problem Practice/Inheritance and Polymorphism: Stack, Queue.cpp
@@ -1,110 +1,117 @@
 #include <iostream>
-using namespace std;
 
-class Node
+class Node 
 {
 public:
-	int mData;
-	Node* mNext;
+    int mData;
+    Node* next;
 
-	Node(int mData) : mData(mData), mNext(nullptr) {}
+    Node(int data) : mData(data), next(nullptr) {}
 };
 
-class LinkedList
+class LinkedList 
 {
 protected:
-	Node* mHead;
+    Node* mHead;
 
 public:
-	LinkedList() : mHead(nullptr) {}
+    LinkedList() : mHead(nullptr) {}
 
-	void Append(int mData)
-	{
-		Node* node = new Node(mData);
-		if(!mHead)
-		{
-			mHead = node;
-		}
-		else
-		{
-			Node* Current = mHead;
-			while (Current->mNext)
-			{
-				Current = Current->mNext;
-			}
-			Current->mNext = node;
-		}
-	}
+    ~LinkedList() 
+    {
+        Node* current = mHead;
+        while (current) 
+        {
+            Node* temp = current;
+            current = current->next;
+            delete temp;
+        }
+    }
 
-	void PrintList()
-	{
-		Node* Current = mHead;
+    void append(int data) 
+    {
+        Node* new_node = new Node(data);
+        if (!mHead) 
+        {
+            mHead = new_node;
+        }
+        else 
+        {
+            Node* current = mHead;
+            while (current->next) 
+            {
+                current = current->next;
+            }
+            current->next = new_node;
+        }
+    }
 
-		while (Current)
-		{
-			cout << Current->mData << " -> ";
-			Current = Current->mNext;
-		}
-		cout << "None" << endl;
-	}
+    void printList() 
+    {
+        Node* current = mHead;
+        while (current) {
+            std::cout << current->mData << " -> ";
+            current = current->next;
+        }
+        std::cout << "None" << std::endl;
+    }
 };
 
-class Stack : public LinkedList
+class Stack : public LinkedList 
 {
 public:
-	void Push(int data)
-	{
-		Append(data);
-	}
+    void push(int data)
+    {
+        append(data);
+    }
 
-	int Pop()
-	{
-		if(!mHead)
-		{
-			return -1;
-		}
-		else if(!mHead->mNext)
-		{
-			int data = mHead->mData;
-			delete mHead;
-			mHead = nullptr;
-			return data;
-		}
-		else 
-		{
-			Node* Current = mHead;
-
-			while (Current->mNext->mNext)
-			{
-				Current = Current->mNext;
-			}
-
-			int data = Current->mNext->mData;
-			delete Current->mNext;
-			Current->mNext = nullptr;
-			return data;
-		}
-	}
+    int pop() 
+    {
+        if (!mHead)
+        {
+            return -1; 
+        }
+        else if (!mHead->next)
+        {
+            int data = mHead->mData;
+            delete mHead;
+            mHead = nullptr;
+            return data;
+        }
+        else 
+        {
+            Node* current = mHead;
+            while (current->next->next) 
+            {
+                current = current->next;
+            }
+            int data = current->next->mData;
+            delete current->next;
+            current->next = nullptr;
+            return data;
+        }
+    }
 };
 
-class Queue : public LinkedList
+class Queue : public LinkedList 
 {
-public: 
-	void EnQueue(int data)
-	{
-		Append(data);
-	}
+public:
+    void enqueue(int data) 
+    {
+        append(data);
+    }
 
-	int DeQueue()
-	{
-		if(!mHead)
-		{
-			return -1;
-		}
-		int data = mHead->mData;
-		Node* temp = mHead;
-		mHead = mHead->mNext;
-		delete temp;
-		return data;
-	}
+    int dequeue() 
+    {
+        if (!mHead) 
+        {
+            return -1; 
+        }
+        int data = mHead->mData;
+        Node* temp = mHead;
+        mHead = mHead->next;
+        delete temp;
+        return data;
+    }
 };
+
