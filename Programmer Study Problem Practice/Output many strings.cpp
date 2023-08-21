4a68dc155222f2fe3888a96adc91d1c9310d1950
performeru <130340605+performeru@users.noreply.github.com>
2023-08-21 19:13:31 +0900

    Update Output many strings.cpp

--- Programmer Study Problem Practice/Output many strings.cpp
+++ Programmer Study Problem Practice/Output many strings.cpp
@@ -3,13 +3,6 @@
 #include <algorithm>
 using namespace std;
 
-bool IS_SameImange(string s, string t)
-{
-	sort(s.begin(), s.end());
-	sort(t.begin(), t.end());
-
-	return s == t;
-}
 
 char MaxLetter(const string& s)
 {
