6271bed78e12f1ce8081fee70c4d2fab0a1c2b6e
performeru <130340605+performeru@users.noreply.github.com>
2023-09-25 20:41:50 +0900

    Update and rename dp to Number of possible paths with Tabulation.cpp

---
+++ C++/DynamicProgramming/Number of possible paths with Tabulation.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int FindWays(int m, int n) 
+{
+	vector<vector<int>> table(m + 1, vector<int>(n + 1, 0));
+
+	table[1][1] = 1;
+
+	for (int row = 0; row <= m; row++)
+  {
+		for (int col = 0; col <= n; col++)
+    {
+			if (row + 1 <= m)
+      {
+				table[row + 1][col] += table[row][col];
+			}
+			if (col + 1 <= n)
+      {
+				table[row][col + 1] += table[row][col];
+			}
+		}
+	}
+
+	return table[m][n];
+}
+
+int main() 
+{
+	cout << FindWays(3, 3) << endl;
+}
