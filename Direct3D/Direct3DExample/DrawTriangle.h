0242518180c08b11e34c897a169daea9e56e77b5
performeru <130340605+performeru@users.noreply.github.com>
2023-11-13 21:09:22 +0900

    Create DrawTriangle.h

---
+++ Direct3D/Direct3DExample/DrawTriangle.h
@@ -0,0 +1,35 @@
+#pragma once
+
+#include <d3dcompiler.h>
+#include "D3DFramework.h"
+
+class DrawTriangle : public D3DFramework
+{
+	struct VERTEX
+	{
+		FLOAT x, y, z;
+		FLOAT U, V;
+	};
+
+	Microsoft::WRL::ComPtr<ID3D11InputLayout> mspInputLayout;
+	Microsoft::WRL::ComPtr<ID3D11Buffer> mspVertexBuffer;
+
+	Microsoft::WRL::ComPtr<ID3D11VertexShader> mspVertexShader;
+	Microsoft::WRL::ComPtr<ID3D11PixelShader> mspPixelShader;
+
+	Microsoft::WRL::ComPtr<ID3D11Texture2D> mspTexture;	// resource
+	Microsoft::WRL::ComPtr<ID3D11ShaderResourceView> mspTextureView;
+
+public:
+	void Initialize(HINSTANCE hInstance, int width = 800, int height = 600) override;
+	void Destroy() override;
+
+private:
+	void InitTriangle();
+	void InitPipeline();
+
+	HRESULT CreateTextureFromBMP();
+
+protected:
+	void Render() override;
+};
